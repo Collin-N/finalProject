1a6abbeb842bc557764694a09fb8414140e10c73
Collin N <bubba9n500@gmail.com>
2017-10-11 09:16:44 -0500

    update

--- finalProject/finalProject/src/finalProject/DungeonEscape.java
+++ finalProject/finalProject/src/finalProject/DungeonEscape.java
@@ -3,8 +3,8 @@ package finalProject;
 public class DungeonEscape {
 
 	public static void main(String[] args) {
-		
-
+		System.out.println("You wake up in an unfamiliar room you have no idea how you got there. You appear to have an empty backpack & amnesia you don’t like where you are now escape from what appears to be a dungeon. You see two paths in front of you. One leads left and the other leads right.");
+//dfghdfgj
 	}
 
 }
